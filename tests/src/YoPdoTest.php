f2775c15c9be1cb5222b3ed353f853e59234ceed
Matt Light <matt.light@lightdatasys.com>
2015-10-29 23:13:49 -0700

    Reduce complexity and duplication of tests

--- tests/src/YoPdoTest.php
+++ tests/src/YoPdoTest.php
@@ -54,32 +54,28 @@ SQL;
      */
     public function testMultipleQueriesCanBeRan($db)
     {
-        $tablename = 'test_multiple_queries_' . uniqid();
-        $populate_sql = <<<SQL
-CREATE TABLE {$tablename} (
-    id INT NOT NULL
-);
-INSERT INTO {$tablename} VALUES (:param_a);
-INSERT INTO {$tablename} VALUES (:param_b);
-INSERT INTO {$tablename} VALUES (:last_param);
+        $table_name = $this->createTable($db);
+        $sql = <<<SQL
+INSERT INTO {$table_name} (a, b) VALUES (:row_1_col_a, :row_1_col_b);
+INSERT INTO {$table_name} (a, b) VALUES (:row_2_col_a, :row_2_col_b);
+INSERT INTO {$table_name} (a, b) VALUES (:last_row_col_a, :last_row_col_b);
 SQL;
-        $result = $db->queryMultiple($populate_sql, array(
-            'param_a'    => 1,
-            'param_b'    => 2,
-            'last_param' => 3,
-        ));
 
-        $select_sql = <<<SQL
-SELECT id AS col
-FROM {$tablename}
-ORDER BY col
-SQL;
-        $result = $db->query($select_sql);
-        $count = 1;
-        while ($row = $result->fetch()) {
-            $this->assertEquals($count, $row['col']);
-            ++$count;
-        }
+        $params = array(
+            'row_1_col_a'    => 20,
+            'row_1_col_b'    => 40,
+            'row_2_col_a'    => 60,
+            'row_2_col_b'    => 30,
+            'last_row_col_a' => 50,
+            'last_row_col_b' => 10,
+        );
+        $db->queryMultiple($sql, $params);
+
+        $this->assertResults($db, $table_name, array(
+            1 => array('a' => $params['row_1_col_a'], 'b' => $params['row_1_col_b']),
+            2 => array('a' => $params['row_2_col_a'],'b' => $params['row_2_col_b']),
+            3 => array('a' => $params['last_row_col_a'], 'b' => $params['last_row_col_b']),
+        ));
     }
 
     /**
@@ -87,32 +83,18 @@ SQL;
      */
     public function testInsert($db)
     {
-        $tablename = 'insert_queries_' . uniqid();
-        $create_table_sql = <<<SQL
-CREATE SEQUENCE {$tablename}_id_seq;
-CREATE TABLE {$tablename} (
-    id INT NOT NULL DEFAULT NEXTVAL('{$tablename}_id_seq'::regclass),
-    other INT NOT NULL
-);
-SQL;
-        $result = $db->queryMultiple($create_table_sql);
+        $rows = array(
+            1 => array('a' => 3, 'b' => 6),
+            2 => array('a' => 2, 'b' => 4),
+            3 => array('a' => 1, 'b' => 2),
+        );
 
-        $db->insert($tablename, array('other' => 3));
-        $db->insert($tablename, array('other' => 2));
-        $db->insert($tablename, array('other' => 1));
-
-        $select_sql = <<<SQL
-SELECT id, other
-FROM {$tablename}
-ORDER BY id
-SQL;
-        $result = $db->query($select_sql);
-        $count = 1;
-        while ($row = $result->fetch()) {
-            $this->assertEquals($count, $row['id']);
-            $this->assertEquals(4 - $count, $row['other']);
-            ++$count;
+        $table_name = $this->createTable($db);
+        foreach ($rows as $row) {
+            $db->insert($table_name, $row);
         }
+
+        $this->assertResults($db, $table_name, $rows);
     }
 
     /**
@@ -120,19 +102,10 @@ SQL;
      */
     public function testLastInsertIdCanBeRetrieved($db)
     {
-        $tablename = 'last_insert_id_' . uniqid();
-        $create_table_sql = <<<SQL
-CREATE SEQUENCE {$tablename}_id_seq;
-CREATE TABLE {$tablename} (
-    id INT NOT NULL DEFAULT NEXTVAL('{$tablename}_id_seq'::regclass),
-    other INT NOT NULL
-);
-SQL;
-        $result = $db->queryMultiple($create_table_sql);
-
+        $table_name = $this->createTable($db);
         for ($i = 1; $i <= 3; $i++) {
-            $db->insert($tablename, array('other' => $i + 5));
-            $this->assertEquals($i, $db->getLastInsertId("{$tablename}_id_seq"));
+            $db->insert($table_name, array('a' => $i + 5, 'b' => $i + 10));
+            $this->assertEquals($i, $db->getLastInsertId("{$table_name}_id_seq"));
         }
     }
 
@@ -141,49 +114,24 @@ SQL;
      */
     public function testUpdateRecordsUsingCustomPlaceholderNames($db)
     {
-        $tablename = 'update_records_' . uniqid();
-        $create_table_sql = <<<SQL
-CREATE SEQUENCE {$tablename}_id_seq;
-CREATE TABLE {$tablename} (
-    id INT NOT NULL,
-    a INT NOT NULL,
-    b INT NOT NULL
-);
-SQL;
-        $result = $db->queryMultiple($create_table_sql);
-
-        $expected = array(
+        $rows = array(
             1 => array('a' => 4, 'b' => 7),
             2 => array('a' => 5, 'b' => 8),
             3 => array('a' => 6, 'b' => 9),
         );
+        $table_name = $this->createPopulatedTable($db, $rows);
 
-        for ($i = 1; $i <= 3; $i++) {
-            $record = $expected[$i];
-            $record['id'] = $i;
-            $db->insert($tablename, $record);
-        }
-
-        $new_expected = $expected;
-        $new_expected[2] = array('a' => 112, 'b' => 112);
+        $expected = $rows;
+        $expected[2] = array('a' => 112, 'b' => 112);
 
         $db->update(
-            $tablename,
+            $table_name,
             array('a' => 'some_number', 'b' => 'some_number'),
             'id = 2',
             array('some_number' => 112)
         );
 
-        $select_sql = <<<SQL
-SELECT id, a, b
-FROM {$tablename}
-ORDER BY id
-SQL;
-        $result = $db->query($select_sql);
-        while ($row = $result->fetch()) {
-            $this->assertEquals($new_expected[$row['id']]['a'], $row['a']);
-            $this->assertEquals($new_expected[$row['id']]['b'], $row['b']);
-        }
+        $this->assertResults($db, $table_name, $expected);
     }
 
     /**
@@ -191,51 +139,29 @@ SQL;
      */
     public function testUpdateRecordsUsingColumnNamesAsPlaceholderNames($db)
     {
-        $tablename = 'update_records_' . uniqid();
-        $create_table_sql = <<<SQL
-CREATE SEQUENCE {$tablename}_id_seq;
-CREATE TABLE {$tablename} (
-    id INT NOT NULL,
-    a INT NOT NULL,
-    b INT NOT NULL
-);
-SQL;
-        $result = $db->queryMultiple($create_table_sql);
-
-        $expected = array(
+        $rows = array(
             1 => array('a' => 4, 'b' => 7),
             2 => array('a' => 5, 'b' => 8),
             3 => array('a' => 6, 'b' => 9),
         );
+        $table_name = $this->createPopulatedTable($db, $rows);
 
-        for ($i = 1; $i <= 3; $i++) {
-            $record = $expected[$i];
-            $record['id'] = $i;
-            $db->insert($tablename, $record);
-        }
-
-        $new_expected = $expected;
-        $new_expected[2] = array('a' => 102, 'b' => 120);
+        $expected = $rows;
+        $expected[2] = array('a' => 102, 'b' => 120);
 
         $db->update(
-            $tablename,
+            $table_name,
             array('a', 'b'),
             'id = 2',
-            $new_expected[2]
+            $expected[2]
         );
 
-        $select_sql = <<<SQL
-SELECT id, a, b
-FROM {$tablename}
-ORDER BY id
-SQL;
-        $result = $db->query($select_sql);
-        while ($row = $result->fetch()) {
-            $this->assertEquals($new_expected[$row['id']]['a'], $row['a']);
-            $this->assertEquals($new_expected[$row['id']]['b'], $row['b']);
-        }
+        $this->assertResults($db, $table_name, $expected);
     }
 
+    /**
+     * @return array
+     */
     public function dbProvider()
     {
         $db = new YoPdo($this->getConfig());
@@ -245,10 +171,85 @@ SQL;
         );
     }
 
+    /**
+     * @return array
+     */
     private function getConfig()
     {
         $config = require __DIR__ . '/../config/config.php';
 
         return $config['database'];
+    }
+
+    /**
+     * @param YoPdo $db
+     * @return string
+     */
+    private function createTable(YoPdo $db)
+    {
+        $table_name = 'test_' . uniqid();
+        $sql = <<<SQL
+CREATE SEQUENCE {$table_name}_id_seq;
+CREATE TABLE {$table_name} (
+    id INT NOT NULL PRIMARY KEY DEFAULT NEXTVAL('{$table_name}_id_seq'::regclass),
+    a INT NOT NULL,
+    b INT NOT NULL
+);
+SQL;
+        $db->queryMultiple($sql);
+
+        return $table_name;
+    }
+
+    /**
+     * @param YoPdo $db
+     * @param $table_name
+     * @param array $rows
+     */
+    private function populateTable(YoPdo $db, $table_name, array $rows)
+    {
+        foreach ($rows as $row) {
+            $db->insert($table_name, $row);
+        }
+    }
+
+    /**
+     * @param YoPdo $db
+     * @param array $rows
+     * @return string
+     */
+    private function createPopulatedTable(YoPdo $db, array $rows)
+    {
+        $table_name = $this->createTable($db);
+        $this->populateTable($db, $table_name, $rows);
+
+        return $table_name;
+    }
+
+    /**
+     * @param YoPdo $db
+     * @param string $table_name
+     * @param array $expected_results
+     * @return array
+     */
+    private function assertResults(YoPdo $db, $table_name, array $expected_results)
+    {
+        $sql = <<<SQL
+SELECT id, a, b
+FROM {$table_name}
+ORDER BY id
+SQL;
+        $result = $db->query($sql);
+        while ($row = $result->fetch()) {
+            if (!array_key_exists('id', $row)) {
+                $this->assertTrue(false, "Field 'id' not found in row.");
+            } else if (!array_key_exists($row['id'], $expected_results)) {
+                $this->assertTrue(false, "Row with key '{$row['id']}' not found in expected results.");
+            } else {
+                $expected_result = $expected_results[$row['id']];
+                $expected_result['id'] = $row['id'];
+                $this->assertEquals($expected_result, $row);
+            }
+        }
     }
 }
