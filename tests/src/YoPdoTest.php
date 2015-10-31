d8c4ae5fcfc0050a686e04044017bc126e2f67df
Matt Light <matt.light@lightdatasys.com>
2015-10-31 01:34:14 -0700

    Rename variable to be more descriptive
    Two characters is not enough

--- tests/src/YoPdoTest.php
+++ tests/src/YoPdoTest.php
@@ -12,24 +12,24 @@ class YoPdoTest extends PHPUnit_Framework_TestCase
     {
         $config = $this->getConfig();
         $pdo = new PDO($config['dsn'], $config['username'], $config['password']);
-        $db = new YoPdo($pdo);
+        $yo_pdo = new YoPdo($pdo);
 
-        $this->assertSame($pdo, $db->getPdo());
+        $this->assertSame($pdo, $yo_pdo->getPdo());
     }
 
     /**
      * @dataProvider dbProvider
      * @expectedException PDOException
      */
-    public function testAnErrorInAQueryThrowsAnException($db)
+    public function testAnErrorInAQueryThrowsAnException($yo_pdo)
     {
-        $db->query('SELECT oops');
+        $yo_pdo->query('SELECT oops');
     }
 
     /**
      * @dataProvider dbProvider
      */
-    public function testASimpleQueryCanBeRan($db)
+    public function testASimpleQueryCanBeRan($yo_pdo)
     {
         $sql = <<<SQL
 SELECT :param_a AS col UNION
@@ -38,7 +38,7 @@ SELECT :last_param AS col
 ORDER BY col
 SQL;
 
-        $result = $db->query($sql, array(
+        $result = $yo_pdo->query($sql, array(
             'param_a'    => 1,
             'param_b'    => 2,
             'last_param' => 3,
@@ -53,9 +53,9 @@ SQL;
     /**
      * @dataProvider dbProvider
      */
-    public function testMultipleQueriesCanBeRan($db)
+    public function testMultipleQueriesCanBeRan($yo_pdo)
     {
-        $table_name = $this->createTable($db);
+        $table_name = $this->createTable($yo_pdo);
         $sql = <<<SQL
 INSERT INTO {$table_name} (a, b) VALUES (:row_1_col_a, :row_1_col_b);
 INSERT INTO {$table_name} (a, b) VALUES (:row_2_col_a, :row_2_col_b);
@@ -70,9 +70,9 @@ SQL;
             'last_row_col_a' => 50,
             'last_row_col_b' => 10,
         );
-        $db->queryMultiple($sql, $params);
+        $yo_pdo->queryMultiple($sql, $params);
 
-        $this->assertResults($db, $table_name, array(
+        $this->assertResults($yo_pdo, $table_name, array(
             1 => array('a' => $params['row_1_col_a'], 'b' => $params['row_1_col_b']),
             2 => array('a' => $params['row_2_col_a'],'b' => $params['row_2_col_b']),
             3 => array('a' => $params['last_row_col_a'], 'b' => $params['last_row_col_b']),
@@ -82,7 +82,7 @@ SQL;
     /**
      * @dataProvider dbProvider
      */
-    public function testInsert($db)
+    public function testInsert($yo_pdo)
     {
         $rows = array(
             1 => array('a' => 3, 'b' => 6),
@@ -90,74 +90,74 @@ SQL;
             3 => array('a' => 1, 'b' => 2),
         );
 
-        $table_name = $this->createTable($db);
+        $table_name = $this->createTable($yo_pdo);
         foreach ($rows as $row) {
-            $db->insert($table_name, $row);
+            $yo_pdo->insert($table_name, $row);
         }
 
-        $this->assertResults($db, $table_name, $rows);
+        $this->assertResults($yo_pdo, $table_name, $rows);
     }
 
     /**
      * @dataProvider dbProvider
      */
-    public function testLastInsertIdCanBeRetrieved($db)
+    public function testLastInsertIdCanBeRetrieved($yo_pdo)
     {
-        $table_name = $this->createTable($db);
+        $table_name = $this->createTable($yo_pdo);
         for ($i = 1; $i <= 3; $i++) {
-            $db->insert($table_name, array('a' => $i + 5, 'b' => $i + 10));
-            $this->assertEquals($i, $db->getLastInsertId("{$table_name}_id_seq"));
+            $yo_pdo->insert($table_name, array('a' => $i + 5, 'b' => $i + 10));
+            $this->assertEquals($i, $yo_pdo->getLastInsertId("{$table_name}_id_seq"));
         }
     }
 
     /**
      * @dataProvider dbProvider
      */
-    public function testUpdateRecordsUsingCustomPlaceholderNames($db)
+    public function testUpdateRecordsUsingCustomPlaceholderNames($yo_pdo)
     {
         $rows = array(
             1 => array('a' => 4, 'b' => 7),
             2 => array('a' => 5, 'b' => 8),
             3 => array('a' => 6, 'b' => 9),
         );
-        $table_name = $this->createPopulatedTable($db, $rows);
+        $table_name = $this->createPopulatedTable($yo_pdo, $rows);
 
         $expected = $rows;
         $expected[2] = array('a' => 112, 'b' => 112);
 
-        $db->update(
+        $yo_pdo->update(
             $table_name,
             array('a' => 'some_number', 'b' => 'some_number'),
             'id = 2',
             array('some_number' => 112)
         );
 
-        $this->assertResults($db, $table_name, $expected);
+        $this->assertResults($yo_pdo, $table_name, $expected);
     }
 
     /**
      * @dataProvider dbProvider
      */
-    public function testUpdateRecordsUsingColumnNamesAsPlaceholderNames($db)
+    public function testUpdateRecordsUsingColumnNamesAsPlaceholderNames($yo_pdo)
     {
         $rows = array(
             1 => array('a' => 4, 'b' => 7),
             2 => array('a' => 5, 'b' => 8),
             3 => array('a' => 6, 'b' => 9),
         );
-        $table_name = $this->createPopulatedTable($db, $rows);
+        $table_name = $this->createPopulatedTable($yo_pdo, $rows);
 
         $expected = $rows;
         $expected[2] = array('a' => 102, 'b' => 120);
 
-        $db->update(
+        $yo_pdo->update(
             $table_name,
             array('a', 'b'),
             'id = 2',
             $expected[2]
         );
 
-        $this->assertResults($db, $table_name, $expected);
+        $this->assertResults($yo_pdo, $table_name, $expected);
     }
 
     /**
@@ -166,10 +166,10 @@ SQL;
     public function dbProvider()
     {
         $factory = new Factory();
-        $db = $factory->createFromConfig($this->getConfig());
+        $yo_pdo = $factory->createFromConfig($this->getConfig());
 
         return array(
-            array($db),
+            array($yo_pdo),
         );
     }
 
@@ -184,10 +184,10 @@ SQL;
     }
 
     /**
-     * @param YoPdo $db
+     * @param YoPdo $yo_pdo
      * @return string
      */
-    private function createTable(YoPdo $db)
+    private function createTable(YoPdo $yo_pdo)
     {
         $table_name = 'test_' . uniqid();
         $sql = <<<SQL
@@ -198,50 +198,50 @@ CREATE TABLE {$table_name} (
     b INT NOT NULL
 );
 SQL;
-        $db->queryMultiple($sql);
+        $yo_pdo->queryMultiple($sql);
 
         return $table_name;
     }
 
     /**
-     * @param YoPdo $db
+     * @param YoPdo $yo_pdo
      * @param $table_name
      * @param array $rows
      */
-    private function populateTable(YoPdo $db, $table_name, array $rows)
+    private function populateTable(YoPdo $yo_pdo, $table_name, array $rows)
     {
         foreach ($rows as $row) {
-            $db->insert($table_name, $row);
+            $yo_pdo->insert($table_name, $row);
         }
     }
 
     /**
-     * @param YoPdo $db
+     * @param YoPdo $yo_pdo
      * @param array $rows
      * @return string
      */
-    private function createPopulatedTable(YoPdo $db, array $rows)
+    private function createPopulatedTable(YoPdo $yo_pdo, array $rows)
     {
-        $table_name = $this->createTable($db);
-        $this->populateTable($db, $table_name, $rows);
+        $table_name = $this->createTable($yo_pdo);
+        $this->populateTable($yo_pdo, $table_name, $rows);
 
         return $table_name;
     }
 
     /**
-     * @param YoPdo $db
+     * @param YoPdo $yo_pdo
      * @param string $table_name
      * @param array $expected_results
      * @return array
      */
-    private function assertResults(YoPdo $db, $table_name, array $expected_results)
+    private function assertResults(YoPdo $yo_pdo, $table_name, array $expected_results)
     {
         $sql = <<<SQL
 SELECT id, a, b
 FROM {$table_name}
 ORDER BY id
 SQL;
-        $result = $db->query($sql);
+        $result = $yo_pdo->query($sql);
         while ($row = $result->fetch()) {
             if (!array_key_exists('id', $row)) {
                 $this->assertTrue(false, "Field 'id' not found in row.");
